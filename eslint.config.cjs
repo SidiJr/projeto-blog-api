7fb3adc9120a50fa74410441b53479bf9efd040b
SidiJr <sednei.jr@gmail.com>
2025-05-31 02:49:07 -0300

    build: :building_construction: Ajustes
    Ajustes.

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,19 +1,30 @@
-module.exports = [
+const js = require("@eslint/js");
+const globals = require("globals");
+const { defineConfig } = require("eslint/config");
+
+module.exports = defineConfig([
   {
-    files: ["**/*.{js,cjs}"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "commonjs",
-      globals: {
-        require: "readonly",
-        module: "readonly",
-      },
-    },
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
     rules: {
-      "prefer-const": "error",
-      "no-console": "warn",
-      "consistent-return": "warn",
-      "prefer-template": "warn",
+      "no-unused-vars": "warn", // Variáveis não usadas emite warning
+      "prefer-const": "error", // Use const se a variável não muda
+      "no-console": "warn", // Alerta se usar console.log
+      "consistent-return": "warn", // Garante funções que retornem algo sempre
+      "prefer-template": "warn", // Prefere template strings em vez de +
     },
   },
-];
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+]);
